075fb9252dc0ab9b96fcf8371847f8b225e79ee0
Your Name <you@example.com>
2022-09-10 18:27:48 -0400

    Create contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
-function Contact () {
-    return (
-        <div>Contact</div>
-    )
+export default function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  return (
+    <section className='container glass-effect col-12 col-sm-10 col-md-8 col-xl-6 mx-auto'>
+      <h2>Contact Me</h2>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="Name"
+            defaultValue={name}
+            onBlur={handleChange}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="Message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          ></textarea>
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <div className='d-flex justify-content-around'>
+            <button className='btn btn-md btn-primary col-6 mt-4 btn-light' type="submit">Submit</button>
+        </div>
+      </form>
+    </section>
+  );
 }
-
-export default Contact;
